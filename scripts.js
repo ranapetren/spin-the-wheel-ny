72e18ac175fc74463d29d1c824f427734f7c2598
Rana Terning <rana.terning@epidemicsound.com>
2024-01-02 17:06:45 +0100

    final version

--- scripts.js
+++ scripts.js
@@ -2,13 +2,29 @@ let wheel = document.querySelector(".wheel");
 let spinBtn = document.querySelector(".spinBtn");
 let value = Math.ceil(Math.random() * 3600);
 
+const tasks = [
+  "Izabella: Gå en catwalk genom rummet",
+  "David och Alex: Dansa tango tillsammans",
+  "Alla: Drick 3 klunkar från ditt glas",
+  "William: Ta en shot",
+  "Ellen och demir: Visa hur ni skulle dansat på ert bröllop",
+  "Sandra och Nils: Sjung en refräng från valfri låt",
+  "Minja och Leone: Visa hur ni skulle överleva en zombieapokalyps med föremål på bordet.",
+  "Hanna: Ta en shot",
+  "Embla: Låt som en katt",
+  "Alex: Visa ditt bästa dance move",
+  "Emma: Välj en färg. Alla som har något med den färgen på sig måste dricka.",
+  "Emilia och Rana: Svara på fråga från Hanna",
+  "Marcel: Svara på fråga från Hanna",
+];
+
 const wheelItems = [
   {
     id: 1,
     name: "item1",
     img: "./assets/william.png",
     type: "task",
-    color: "linear-gradient(#e66465, #9198e5)",
+    color: "linear-gradient(#c7005c, #9198e5)",
   },
   {
     id: 2,
@@ -22,7 +38,7 @@ const wheelItems = [
     name: "item3",
     type: "task",
     img: "./assets/demir.png",
-    color: "linear-gradient(#e66465, #9198e5)",
+    color: "linear-gradient(#c7005c, #9198e5)",
   },
   {
     id: 4,
@@ -36,7 +52,7 @@ const wheelItems = [
     name: "item5",
     type: "task",
     img: "./assets/alex.png",
-    color: "linear-gradient(#e66465, #9198e5)",
+    color: "linear-gradient(#c7005c, #9198e5)",
   },
   {
     id: 6,
@@ -50,11 +66,39 @@ const wheelItems = [
     name: "item7",
     type: "task",
     img: "./assets/david.png",
-    color: "linear-gradient(#e66465, #9198e5)",
+    color: "linear-gradient(#c7005c, #9198e5)",
   },
   {
     id: 8,
     name: "item8",
+    type: "drink",
+    color:
+      "linear-gradient(to right top, #ff6f01, #f79725, #f0b84e, #ecd47b, #edecac)",
+  },
+  {
+    id: 9,
+    name: "item9",
+    type: "task",
+    img: "./assets/nils.png",
+    color: "linear-gradient(#c7005c, #9198e5)",
+  },
+  {
+    id: 10,
+    name: "item10",
+    type: "drink",
+    color:
+      "linear-gradient(to right top, #ff6f01, #f79725, #f0b84e, #ecd47b, #edecac)",
+  },
+  {
+    id: 11,
+    name: "item11",
+    type: "task",
+    img: "./assets/marcel.png",
+    color: "linear-gradient(#c7005c, #9198e5)",
+  },
+  {
+    id: 12,
+    name: "item12",
     type: "drink",
     color:
       "linear-gradient(to right top, #ff6f01, #f79725, #f0b84e, #ecd47b, #edecac)",
@@ -69,15 +113,17 @@ for (let i = 0; i < wheelItems.length; i++) {
   div.setAttribute("style", `--i:${i}; --clr:${item.color}`);
 
   if (item.img) {
-    div.innerHTML = `<div id="task"><img src="${item.img}" />${item.id}</div>`;
+    div.innerHTML = `<div id="task"><img src="${item.img}" /></div>`;
   } else {
-    div.innerHTML = `<div id="fireball">${item.id}</div>`;
+    div.innerHTML = `<div id="fireball"></div>`;
   }
 
   wheel.appendChild(div);
 }
 
 function spin() {
+  document.getElementById("result").style.visibility = "hidden";
+
   const numberOfItems = wheelItems.length;
   const degreesPerItem = 360 / numberOfItems; // 45 degrees for each item if there are 8 items
 
@@ -99,15 +145,40 @@ function spin() {
   }
 
   // Add extra rotations for a dramatic effect
-  const extraRotations = 360 * (3 + Math.floor(Math.random() * 5)); // 3 to 5 extra full rotations
+  const extraRotations = 360 * (3 + Math.floor(Math.random() * 5));
   const finalAngle = value + angleToRotate + extraRotations;
 
   // Update the wheel
   wheel.style.transform = "rotate(" + finalAngle + "deg)";
   value = finalAngle; // Update the current value
 
-  // Output the selected item
-  console.log("Selected Item Index:", selectedItemIndex + 1); // +1 for display purposes
+  if (wheelItems[selectedItemIndex].type === "task") {
+    const chosenTask = tasks[Math.floor(Math.random() * tasks.length)];
+
+    document.getElementById(
+      "result"
+    ).innerHTML = `<div><h1>${chosenTask}</h1></>`;
+
+    setTimeout(() => {
+      // document.getElementById("result").style.visibility = "visible";
+      showDiv();
+      document.getElementById(
+        "result"
+      ).innerHTML = `<div><h1>${chosenTask}</h1></>`;
+    }, 5500);
+  }
+}
+
+function showDiv() {
+  var div = document.getElementById("result");
+  div.style.visibility = "hidden"; // Temporarily hide
+
+  // Remove the element and reinsert it
+  div.parentNode.removeChild(div);
+  setTimeout(function () {
+    document.body.appendChild(div);
+    div.style.visibility = "visible"; // Make visible
+  }, 10); // Short timeout
 }
 
 spinBtn.onclick = function () {
